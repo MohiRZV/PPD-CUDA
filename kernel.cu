4d83bb42051dcba0ccbd8e657e67661e7f67ee0c
MohiRZV <rsalsigan@gmail.com>
2022-01-10 21:30:26 +0200

    added second variation of laplace filter

--- kernel.cu
+++ kernel.cu
@@ -11,9 +11,9 @@ using namespace std;
 
 #define THREADS_NO 512
 
-cudaError_t laplaceFilterWithCuda(unsigned char* matrix, unsigned char* result_matrix, float* filter, int rows, int columns);
+cudaError_t filterWithCuda(unsigned char* matrix, unsigned char* result_matrix, float* filter, int rows, int columns);
 
-__global__ void laplaceFilterKernel(unsigned char* matrix, unsigned char* result, float* filter, int rows, int columns, int filter_rows, int filter_columns) {
+__global__ void filterKernel(unsigned char* matrix, unsigned char* result, float* filter, int rows, int columns, int filter_rows, int filter_columns) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     if (index < rows * columns) {
         // get row and column index from liniarised matrix index
@@ -64,7 +64,7 @@ __global__ void laplaceFilterKernel(unsigned char* matrix, unsigned char* result
             filterColumn = filterStartColumn;
         }
         // save computed result
-        result[row * rows + column] = (char)sum / (filter_columns * filter_rows);
+        result[row * rows + column] = (int)sum / (filter_columns * filter_rows);
     }
 
 }
@@ -82,7 +82,7 @@ void filter_laplacian_init(float* filter) {
     filter[8] = 0;
 }
 
-void filter_blur_init(float* filter) {
+void filter_laplacian2_init(float* filter) {
     //float factor = 1;
     //filter[0] = factor * 0;
     //filter[1] = factor * -1;
@@ -95,16 +95,19 @@ void filter_blur_init(float* filter) {
     //filter[8] = factor * 0;
     float factor = 1;
     filter[0] = factor * 1;
-    filter[1] = factor * 2;
+    filter[1] = factor * 1;
     filter[2] = factor * 1;
-    filter[3] = factor * 2;
-    filter[4] = factor * 4;
-    filter[5] = factor * 2;
+    filter[3] = factor * 1;
+    filter[4] = factor * 8;
+    filter[5] = factor * 1;
     filter[6] = factor * 1;
-    filter[7] = factor * 2;
+    filter[7] = factor * 1;
     filter[8] = factor * 1;
 }
-
+// 1 - apply second gaussian filter on rgb image
+// 2 - apply first gaussian filter on graysclale image
+// 0 - apply 1 then 2
+int filter_type = 2;
 int main()
 {
     // declare input and output openCV matrixes
@@ -117,7 +120,7 @@ int main()
 
     // input, output files
     string input_file{ "image.jpg" };
-    string output_file{ "output.jpg" };
+    string output_file{ "output.jpg"};
     string image_path = cv::samples::findFile(input_file);
     cv::cvtColor(cv::imread(image_path, 1), inputImageRGBA, 2);
 
@@ -144,30 +147,88 @@ int main()
 
     float* filter = new float[3 * 3];
 
-    for (size_t i = 0; i < numRows * numCols; ++i) {
-        uchar4 rgba = inputImageRGBAMatrix[i];
-        // convert to grayscale
-        grayscale[i] = 0.2989 * rgba.x + 0.5870 * rgba.y + 0.1140 * rgba.z;
+    cudaError_t cudaStatus;
+    
+    // ########### apply second laplacian kernel filter ###########
+    if (filter_type == 1 || filter_type == 0) {
+        unsigned char* red = new unsigned char[numPixels];
+        unsigned char* blue = new unsigned char[numPixels];
+        unsigned char* green = new unsigned char[numPixels];
+
+        unsigned char* redBlurred = new unsigned char[numPixels];
+        unsigned char* blueBlurred = new unsigned char[numPixels];
+        unsigned char* greenBlurred = new unsigned char[numPixels];
+
+        filter_laplacian2_init(filter);
+
+        for (size_t i = 0; i < numRows * numCols; ++i) {
+            uchar4 rgba = inputImageRGBAMatrix[i];
+            red[i] = rgba.x;
+            green[i] = rgba.y;
+            blue[i] = rgba.z;
+        }
+
+        cudaStatus = filterWithCuda(red, redBlurred, filter, numRows, numCols);
+        if (cudaStatus != cudaSuccess) {
+            fprintf(stderr, "filterWithCuda failed!");
+            return 1;
+        }
+        cudaStatus = filterWithCuda(green, greenBlurred, filter, numRows, numCols);
+        if (cudaStatus != cudaSuccess) {
+            fprintf(stderr, "filterWithCuda failed!");
+            return 1;
+        }
+        cudaStatus = filterWithCuda(blue, blueBlurred, filter, numRows, numCols);
+
+        if (cudaStatus != cudaSuccess) {
+            fprintf(stderr, "filterWithCuda failed!");
+            return 1;
+        }
+
+        for (size_t i = 0; i < numRows * numCols; ++i) {
+            uchar4 rgba = make_uchar4(redBlurred[i], greenBlurred[i], blueBlurred[i], 255);
+            outputImageRGBAMatrix[i] = rgba;
+        }
+
+        memcpy((uchar4*)outputImageRGBA.ptr<unsigned char>(0), outputImageRGBAMatrix, numRows * numCols * sizeof(uchar4));
+
+        delete[] red;
+        delete[] green;
+        delete[] blue;
+
+        delete[] redBlurred;
+        delete[] greenBlurred;
+        delete[] blueBlurred;
     }
 
-    filter_laplacian_init(filter);
-    cudaError_t cudaStatus = laplaceFilterWithCuda(grayscale, laplacian, filter, numRows, numCols);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "laplaceFilterWithCuda failed!");
-        return 1;
-    }
+    // ########### apply first laplacian filter ###########
+    if (filter_type == 2 || filter_type == 0) {
+        for (size_t i = 0; i < numRows * numCols; ++i) {
+            uchar4 rgba = outputImageRGBAMatrix[i];
+            if(filter_type==2)
+                rgba = inputImageRGBAMatrix[i];
+            // convert to grayscale
+            grayscale[i] = 0.2989 * rgba.x + 0.5870 * rgba.y + 0.1140 * rgba.z;
+        }
 
+        filter_laplacian_init(filter);
+        cudaStatus = filterWithCuda(grayscale, laplacian, filter, numRows, numCols);
+        if (cudaStatus != cudaSuccess) {
+            fprintf(stderr, "filterWithCuda failed!");
+            return 1;
+        }
+
+        for (size_t i = 0; i < numRows * numCols; ++i) {
+            uchar4 rgba = make_uchar4(laplacian[i], laplacian[i], laplacian[i], 255);
+            outputImageRGBAMatrix[i] = rgba;
+        }
+    }
     // cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
     cudaStatus = cudaDeviceReset();
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaDeviceReset failed!");
         return 1;
-    }
-
-    for (size_t i = 0; i < numRows * numCols; ++i) {
-        uchar4 rgba = make_uchar4(laplacian[i], laplacian[i], laplacian[i], 255);
-        outputImageRGBAMatrix[i] = rgba;
     }
 
     memcpy((uchar4*)outputImageRGBA.ptr<unsigned char>(0), outputImageRGBAMatrix, numRows * numCols * sizeof(uchar4));
@@ -188,8 +249,9 @@ int main()
     return 0;
 }
 
-cudaError_t laplaceFilterWithCuda(unsigned char* matrix, unsigned char* result_matrix, float* filter, int rows, int columns)
+cudaError_t filterWithCuda(unsigned char* matrix, unsigned char* result_matrix, float* filter, int rows, int columns)
 {
+    // device matrixes
     unsigned char* dev_matrix = 0;
     unsigned char* dev_result = 0;
     float* dev_filter = 0;
@@ -201,13 +263,8 @@ cudaError_t laplaceFilterWithCuda(unsigned char* matrix, unsigned char* result_m
         goto Error;
     }
 
+    // alloc memory for device variables
     cudaStatus = cudaMalloc((void**)&dev_result, rows * columns * sizeof(unsigned char));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_matrix, rows * columns * sizeof(unsigned char));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
@@ -219,22 +276,33 @@ cudaError_t laplaceFilterWithCuda(unsigned char* matrix, unsigned char* result_m
         goto Error;
     }
 
+    cudaStatus = cudaMalloc((void**)&dev_matrix, rows * columns * sizeof(unsigned char));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    // copy host matrix into device matrix
     cudaStatus = cudaMemcpy(dev_matrix, matrix, rows * columns * sizeof(unsigned char), cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
     }
 
+    // copy host result matrix into device result
     cudaStatus = cudaMemcpy(dev_result, result_matrix, rows * columns * sizeof(unsigned char), cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
     }
 
+    // copy host kernel into device filter
     cudaStatus = cudaMemcpy(dev_filter, filter, 3 * 3 * sizeof(float), cudaMemcpyHostToDevice);
 
+    // split pixels into THREAD_NO blocks
     int blocksNo = rows * columns / (THREADS_NO - 1);
-    laplaceFilterKernel << <blocksNo, THREADS_NO >> > (dev_matrix, dev_result, dev_filter, rows, columns, 3, 3);
+    // launch filtering on GPU
+    filterKernel << <blocksNo, THREADS_NO >> > (dev_matrix, dev_result, dev_filter, rows, columns, 3, 3);
 
     cudaStatus = cudaGetLastError();
     if (cudaStatus != cudaSuccess) {
@@ -244,10 +312,11 @@ cudaError_t laplaceFilterWithCuda(unsigned char* matrix, unsigned char* result_m
 
     cudaStatus = cudaDeviceSynchronize();
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching laplaceFilterKernel!\n", cudaStatus);
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching filterKernel!\n", cudaStatus);
         goto Error;
     }
 
+    // copy device result to host result matrix
     cudaStatus = cudaMemcpy(result_matrix, dev_result, rows * columns * sizeof(unsigned char), cudaMemcpyDeviceToHost);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
